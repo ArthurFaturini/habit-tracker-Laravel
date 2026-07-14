977b78d7745b6bb850ed9b72535e40cab94edf63
ArthurFaturini <arthurfaturinirpa@gamil.com>
2026-07-14 16:17:12 -0300

    feat(User): setup migrations/model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+// Forma nova:
+// #[Fillable(['name', 'email', 'password'])]
+// #[Hidden(['password'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -25,8 +36,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password' => 'hashed'
         ];
     }
 }
