5c876cd89b45a366986224b729f3985779259e3b
ArthurFaturini <arthurfaturinirpa@gamil.com>
2026-07-16 15:30:12 -0300

    feat: create logout route and improve logic

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,9 +9,17 @@ class SiteController extends Controller
 
         $name = "Arthur";
         $habits = ['Ler', 'Correr', "Estudar"];
-        return view('home', [
-            'name' => $name,
-            'habits' => $habits
-        ]);
+        //Forma completa:
+        // return view('home', [
+        //     'name' => $name,
+        //     'habits' => $habits
+        // ]);
+        //Forma Reduzida(Só funciona se os nomes forem iguais):
+        return view('home', compact('name', 'habits'));
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use App\Http\Controllers\Auth\LoginController;
 //     return '<h1>Hello, World!<h1>';
 // });
 
-Route::get('/', [SiteController::class, 'index']);
+// SITE
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
 
-Route::get('/login', [LoginController::class, "index"]);
-Route::post('/login', [LoginController::class, "authenticate"]);
+// LOGIN
+Route::get('/login', [LoginController::class, "index"])->name('site.login');
+Route::post('/login', [LoginController::class, "authenticate"])->name('auth.login');
+
+// AUTH
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
+    Route::post('/logout', [LoginController::class, "logout"])->name('auth.logout');
+});
